f093cac5e6826a09ee40dacb904e13c6e03563ba
aidencrilley <aidencri@udel.edu>
2023-05-08 13:03:07 -0400

    adding navbar file, images file

---
+++ src/Navbar.tsx
@@ -0,0 +1,13 @@
+function Navbar() {
+    return (
+        <nav className="nav">
+            <ul>
+                <li><a href="/home">HOME</a></li>
+                <li><a href="/about">ABOUT</a></li>
+                <li><a href="/login">LOGIN</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
