dfecc352928cc928582614f5e56f1be965e0ffa2
aidencrilley <aidencri@udel.edu>
2023-05-08 12:59:09 -0400

    adding files/logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Navbar";
+import meandmom from "./images/meandmom.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      
+        <h1>$ whoami</h1>
+        <div className="container">
+          <div className="image">
+            <img src={meandmom} id="profpic" alt="Mom and I"/>
+          </div>
+          <div id="about">
+          <blockquote>
+            <p id="whoami">
+              My name is Aiden Crilley and I am from Port Washington, New York. I am a senior computer science at the University of Delaware.
+              Upon graduation I will be working as a software engineer at <a href="https://www.pico.net/">Pico</a>.
+              I am a huge New York Rangers fan and I enjoy watching baseball occassionally as well.
+            </p>
+          </blockquote>
+          </div>
+        </div>
+     
     </div>
   );
 }
